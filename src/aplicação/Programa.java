83616ed3557928c0043d04eb8b23c8115a3b0bac
Pedro Henrique Sobrinho Gamarra da Silva <Pedrohhouro@gmail.com>
2020-10-01 20:42:20 -0300

    Entidade Vendedor

--- src/aplicação/Programa.java
+++ src/aplicação/Programa.java
@@ -1,13 +1,18 @@
 package aplicação;
 
+import java.util.Date;
+
 import entidade.model.Departamento;
+import entidade.model.Vendedor;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
 		Departamento obj = new Departamento(1, "Livros");
-		System.out.println(obj);
+		Vendedor vendedor = new Vendedor(21, "BOB", "Bob@gmail.com", new Date(), 3000.0, obj);
+		System.out.println(vendedor);
+		
 
 	}
 
