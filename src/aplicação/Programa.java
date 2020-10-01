82031df36587b817fccbeeb65899ff4dfd1a3d51
Pedro Henrique Sobrinho Gamarra da Silva <Pedrohhouro@gmail.com>
2020-10-01 20:52:44 -0300

    FabricaDao e VendedorJBDC

--- src/aplicação/Programa.java
+++ src/aplicação/Programa.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 import entidade.model.Departamento;
 import entidade.model.Vendedor;
+import model.dao.FabricaDao;
+import model.dao.VendedorDao;
 
 public class Programa {
 
@@ -11,6 +13,9 @@ public class Programa {
 		
 		Departamento obj = new Departamento(1, "Livros");
 		Vendedor vendedor = new Vendedor(21, "BOB", "Bob@gmail.com", new Date(), 3000.0, obj);
+		
+		VendedorDao vendedorDao = FabricaDao.criarVendedorDao();
+		
 		System.out.println(vendedor);
 		
 
---
+++ src/model/dao/impl/VendedorDaoJDBC.java
@@ -0,0 +1,33 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import entidade.model.Vendedor;
+import model.dao.VendedorDao;
+
+public class VendedorDaoJDBC implements VendedorDao {
+
+	@Override
+	public void inserir(Vendedor obj) {
+	}
+
+	@Override
+	public void atualizar(Vendedor obj) {
+	}
+
+	@Override
+	public void deletarPorId(Integer id) {
+	}
+
+	@Override
+	public Vendedor acharPorId(Integer id) {
+		return null;
+	}
+
+	@Override
+	public List<Vendedor> acharTodos() {
+		return null;
+	}
+	
+
+}
