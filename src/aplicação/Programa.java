092fc8f13e4b99a17330ec795632c79da37f349b
Pedro Henrique Sobrinho Gamarra da Silva <Pedrohhouro@gmail.com>
2020-10-01 20:34:43 -0300

    Entidade do Departamento

---
+++ src/aplicação/Programa.java
@@ -0,0 +1,14 @@
+package aplicação;
+
+import entidade.model.Departamento;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Departamento obj = new Departamento(1, "Livros");
+		System.out.println(obj);
+
+	}
+
+}
