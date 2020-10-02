4f274dfe095a828893f0d3f938f33df9d0547591
Pedro Henrique Sobrinho Gamarra da Silva <Pedrohhouro@gmail.com>
2020-10-01 21:22:29 -0300

    Implementação da Busca por ID

--- src/aplicação/Programa.java
+++ src/aplicação/Programa.java
@@ -1,8 +1,5 @@
 package aplicação;
 
-import java.util.Date;
-
-import entidade.model.Departamento;
 import entidade.model.Vendedor;
 import model.dao.FabricaDao;
 import model.dao.VendedorDao;
@@ -11,10 +8,9 @@ public class Programa {
 
 	public static void main(String[] args) {
 		
-		Departamento obj = new Departamento(1, "Livros");
-		Vendedor vendedor = new Vendedor(21, "BOB", "Bob@gmail.com", new Date(), 3000.0, obj);
-		
 		VendedorDao vendedorDao = FabricaDao.criarVendedorDao();
+		
+		Vendedor vendedor = vendedorDao.acharPorId(3);
 		
 		System.out.println(vendedor);
 		
--- src/model/dao/impl/VendedorDaoJDBC.java
+++ src/model/dao/impl/VendedorDaoJDBC.java
@@ -1,11 +1,24 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
+import entidade.model.Departamento;
 import entidade.model.Vendedor;
 import model.dao.VendedorDao;
 
 public class VendedorDaoJDBC implements VendedorDao {
+	
+	private Connection conexao;
+	
+	public VendedorDaoJDBC(Connection conexao) {
+		this.conexao = conexao;
+	}
 
 	@Override
 	public void inserir(Vendedor obj) {
@@ -21,9 +34,41 @@ public class VendedorDaoJDBC implements VendedorDao {
 
 	@Override
 	public Vendedor acharPorId(Integer id) {
-		return null;
+		PreparedStatement statement = null;
+		ResultSet resultset = null;
+		try {
+			statement = conexao.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "WHERE seller.Id = ? ");
+			
+			statement.setInt(1, id);
+			resultset = statement.executeQuery();
+			
+		if (resultset.next()) {
+			Departamento departamento = new Departamento();
+			departamento.setId(resultset.getInt("DepartmentId"));
+			departamento.setNome(resultset.getString("DepName"));
+			Vendedor obj = new Vendedor();
+			obj.setId(resultset.getInt("Id"));
+			obj.setNome(resultset.getString("Name"));
+			obj.setEmail(resultset.getString("Email"));
+			obj.setSalarioBase(resultset.getDouble("BaseSalary"));
+			obj.setDataDeNascimento(resultset.getDate("BirthDate"));
+			obj.setDepartamento(departamento);
+			return obj;
+			
+		}return null;
+			
+		}catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(statement);
+			DB.closeResultSet(resultset);
+		}
 	}
-
 	@Override
 	public List<Vendedor> acharTodos() {
 		return null;
